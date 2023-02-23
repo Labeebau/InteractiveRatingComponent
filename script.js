9114dac4be2e44a1c38e10387d86bc813acd4656
Labeeba <77605700+Labeebau@users.noreply.github.com>
2023-02-24 00:43:12 +0530

    Added interaction

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-var btn=document.getElementById("btnSubmit");
-btn.addEventListener("click",function(){
-    alert("submitted");
-   // document.getElementsByClassName("rate").visibility="hidden";
-    document.getElementsByClassName("thank").visibility="visible";
+var rating = 0;
+document.querySelectorAll("#number").forEach(element => {
+    element.addEventListener("click", function () {
+        document.querySelectorAll("#number").forEach(elm => {
+            elm.classList.remove("pressed");
+        });
+        element.classList.add("pressed");
+        rating = element.innerHTML;
+    });
+})
+var btnSubmit = document.getElementById("btnSubmit");
 
+btnSubmit.addEventListener("click", function () {
+    if (rating === 0)
+        document.getElementById("error").innerHTML = "Please select a rating !!!";
+    else {
+
+        document.getElementById("rate").classList.add("hidden");
+        document.getElementById("thank").classList.remove("hidden");
+        document.getElementById("thank").classList.add("active");
+        document.getElementById("rating").innerHTML = "You selected " + rating + " out of 5";
+    }
 });
+
